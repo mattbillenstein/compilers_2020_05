32a65618ef5884d5dd1a956d8d8873181e00331f
Dan Davison <dandavison7@gmail.com>
2020-05-04 16:15:54 -0400

    Refactor

--- wabbit/model.py
+++ wabbit/model.py
@@ -6,26 +6,46 @@ class Node:
     def __repr__(self):
         return f"{self.__class__.__name__}({self.__dict__})"
 
+    @property
+    def tokens(self):
+        raise NotImplementedError
+
 
 @dataclass
 class Integer(Node):
     value: int
+
+    @property
+    def tokens(self):
+        return [str(self.value)]
 
 
 @dataclass
 class Float(Node):
     value: float
 
+    @property
+    def tokens(self):
+        return [str(self.value)]
+
 
 @dataclass
 class Name(Node):
     name: str
+
+    @property
+    def tokens(self):
+        return [self.name]
 
 
 @dataclass
 class UnaryOp(Node):
     op: str
     right: Any
+
+    @property
+    def tokens(self):
+        return [self.op, *self.right.tokens]
 
 
 @dataclass
@@ -34,12 +54,20 @@ class BinOp(Node):
     left: Any
     right: Any
 
+    @property
+    def tokens(self):
+        return [*self.left.tokens, self.op, *self.right.tokens]
+
 
 @dataclass
 class ConstDef(Node):
     name: str
     value: Union[int, float]
     type: str = None
+
+    @property
+    def tokens(self):
+        return ["const", self.name if self.name is not None else "", str(self.value)]
 
 
 @dataclass
@@ -48,16 +76,31 @@ class VarDef(Node):
     type: str
     value: Any = None
 
+    @property
+    def tokens(self):
+        tokens = ["var", self.name, self.type]
+        if self.value is not None:
+            tokens.extend(["=", str(self.value)])
+        return tokens
+
 
 @dataclass
 class Assignment(Node):
     location: Any
     value: Any
 
+    @property
+    def tokens(self):
+        return [self.location, "=", *self.value.tokens]
+
 
 @dataclass
 class Print(Node):
     expression: Any
+
+    @property
+    def tokens(self):
+        return ["print", *self.expression.tokens]
 
 
 @dataclass
@@ -66,53 +109,19 @@ class If(Node):
     thn: Any
     els: Any
 
+    @property
+    def tokens(self):
+        return [
+            "if",
+            *self.tst.tokens,
+            "{",
+            *self.thn.tokens,
+            "}",
+            "{",
+            *self.els.tokens,
+            "}",
+        ]
+
 
 def to_source(program):
-    return "\n".join(_to_source(stmnt) + ";" for stmnt in program or [])
-
-
-def _to_source(node):
-
-    if isinstance(node, (Integer, Float)):
-        return str(node.value)
-
-    elif isinstance(node, UnaryOp):
-        return "".join([node.op, _to_source(node.right)])
-
-    elif isinstance(node, BinOp):
-        return " ".join([_to_source(node.left), node.op, _to_source(node.right)])
-
-    elif isinstance(node, Name):
-        return node.name
-
-    elif isinstance(node, ConstDef):
-        return " ".join(["const", node.name if node.name is not None else "", str(node.value)])
-
-    elif isinstance(node, VarDef):
-        tokens = ["var", node.name, node.type]
-        if node.value is not None:
-            tokens.extend(["=", str(node.value)])
-        return " ".join(tokens)
-
-    elif isinstance(node, Assignment):
-        return " ".join([node.location, "=", _to_source(node.value)])
-
-    elif isinstance(node, If):
-        return " ".join(
-            [
-                "if",
-                _to_source(node.tst),
-                "{",
-                _to_source(node.thn),
-                "}",
-                "{",
-                _to_source(node.els),
-                "}",
-            ]
-        )
-
-    elif isinstance(node, Print):
-        return " ".join(["print", _to_source(node.expression)])
-
-    else:
-        raise RuntimeError(f"Can't convert {node} to source")
+    return "\n".join(" ".join(node.tokens) + ";" for node in program or [])
