f9f1ee5fc6c5f315aae0284f3f9048304ce7572d
Dan Davison <dandavison7@gmail.com>
2020-05-04 23:11:21 -0400

    Format model as JSON
    This is intended for debugging, e.g. with a browser extension like JSONView

---
+++ wabbit/format_json.py
@@ -0,0 +1,32 @@
+import json
+
+from typeguard import typechecked
+
+from wabbit.model import Node
+
+
+class NodeJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Node):
+            return repr(obj)
+
+
+class TreeFormatter:
+    """
+    Format model as JSON-encodable tree.
+    """
+
+    @typechecked
+    def visit(self, node: Node, **kwargs):
+        data = node.__dict__.copy()
+        for child in ["left", "right"]:
+            if hasattr(node, child):
+                data[child] = self.visit(getattr(node, child))
+        if hasattr(node, "statements"):
+            data["statements"] = [self.visit(s) for s in node.statements]
+        data["type"] = node.__class__.__name__
+        return data
+
+
+def format_json(node):
+    return json.dumps(TreeFormatter().visit(node), indent=2, cls=NodeJSONEncoder)
