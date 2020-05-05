c48ef729fe0e22567e8872098e3cbb7b802854ad
Dan Davison <dandavison7@gmail.com>
2020-05-05 09:38:34 -0400

    Add more type annotations
    mypy found a bug

--- wabbit/format_source.py
+++ wabbit/format_source.py
@@ -1,3 +1,18 @@
+from wabbit.model import (
+    Assign,
+    BinOp,
+    ConstDef,
+    Float,
+    If,
+    Integer,
+    Name,
+    Print,
+    UnaryOp,
+    VarDef,
+    While,
+    Statements,
+)
+
 class SourceFormatter:
     """
     Format model as source code.
@@ -7,42 +22,42 @@ class SourceFormatter:
         method_name = "visit_" + node.__class__.__name__
         return getattr(self, method_name)(node, **kwargs)
 
-    def visit_Statements(self, node, level=0):
+    def visit_Statements(self, node: Statements, level=0) -> str:
         indent = " " * 4 * level
         return "\n".join(indent + self.visit(stmnt) + ";" for stmnt in node.statements)
 
-    def visit_Float(self, node):
+    def visit_Float(self, node: Float) -> str:
         return str(node.value)
 
-    def visit_Integer(self, node):
+    def visit_Integer(self, node: Integer) -> str:
         return str(node.value)
 
-    def visit_Name(self, node):
+    def visit_Name(self, node: Name) -> str:
         return node.name
 
-    def visit_UnaryOp(self, node):
+    def visit_UnaryOp(self, node: UnaryOp) -> str:
         return node.op + self.visit(node.right)
 
-    def visit_BinOp(self, node):
+    def visit_BinOp(self, node: BinOp) -> str:
         return " ".join([self.visit(node.left), node.op, self.visit(node.right)])
 
-    def visit_ConstDef(self, node):
-        tokens = ["const", node.name, "="] + (node.type or []) + [str(node.value)]
+    def visit_ConstDef(self, node: ConstDef) -> str:
+        tokens = ["const", node.name, "="] + ([node.type] if node.type else []) + [str(node.value)]
         return " ".join(tokens)
 
-    def visit_VarDef(self, node):
+    def visit_VarDef(self, node: VarDef) -> str:
         tokens = ["var", node.name, node.type]
         if node.value is not None:
             tokens.extend(["=", str(node.value)])
         return " ".join(tokens)
 
-    def visit_Assign(self, node):
+    def visit_Assign(self, node: Assign) -> str:
         return " ".join([node.location, "=", self.visit(node.value)])
 
-    def visit_Print(self, node):
+    def visit_Print(self, node: Print) -> str:
         return " ".join(["print", self.visit(node.expression)])
 
-    def visit_If(self, node):
+    def visit_If(self, node: If) -> str:
         # TODO: hard-coded indent=1
         return f"""\
 if {self.visit(node.test)} {{
@@ -51,7 +66,7 @@ if {self.visit(node.test)} {{
 {self.visit(node.else_, level=1)}
 }}
 """
-    def visit_While(self, node):
+    def visit_While(self, node: While) -> str:
         # TODO: hard-coded indent=1
         return f"""\
 while {self.visit(node.test)} {{
@@ -59,5 +74,5 @@ while {self.visit(node.test)} {{
 }}
 """
 
-def format_source(node):
+def format_source(node: Statements) -> str:
     return SourceFormatter().visit(node, level=0)
