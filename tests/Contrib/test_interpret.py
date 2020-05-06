a9a226b4612fd05ae0d5708816cc01ccc02da3b6
André Roberge <andre.roberge@gmail.com>
2020-05-06 08:30:53 -0300

    Updated interpreter tests (using pytest) to make use of ChainMap and get proper scope working.

--- tests/Contrib/test_interpret.py
+++ tests/Contrib/test_interpret.py
@@ -1,6 +1,7 @@
 # test_script_models.py
 # uses pytest
 
+from collections import ChainMap
 import os
 import sys
 
@@ -12,37 +13,43 @@ from wabbit.interp import interpret
 
 
 def test_expr1():
-    env = {'capture_print': []}
+    env = ChainMap()
+    env['capture_print'] = []
     assert interpret(sm.expr_model, {}) == 14
     interpret(sm.expr_model_a, env)
     assert env['capture_print'] == [14]
 
 
 def test_model1():
-    env = {'capture_print': []}
+    env = ChainMap()
+    env['capture_print'] = []
     interpret(sm.model1, env)
     assert env['capture_print'] == [-10, 2.75, 1, 2]
 
 
 def test_model2():
-    env = {'capture_print': []}
+    env = ChainMap()
+    env['capture_print'] = []
     interpret(sm.model2, env)
     assert env['capture_print'] == [6.28318]
 
 
 def test_model3():
-    env = {'capture_print': []}
+    env = ChainMap()
+    env['capture_print'] = []
     interpret(sm.model3, env)
     assert env['capture_print'] == [2]
 
 
 def test_model4():
-    env = {'capture_print': []}
+    env = ChainMap()
+    env['capture_print'] = []
     interpret(sm.model4, env)
     assert env['capture_print'] == [1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
 
 
 def test_model5():
-    env = {'capture_print': []}
+    env = ChainMap()
+    env['capture_print'] = []
     interpret(sm.model5, env)
     assert env['capture_print'] == [42, 37]
