64c1ed5bd517c333d47eddf1b789faab730b14ea
Dan Davison <dandavison7@gmail.com>
2020-05-06 00:18:39 -0400

    Make blocks have local scope

--- wabbit/interp.py
+++ wabbit/interp.py
@@ -55,58 +55,59 @@ class Interpreter:
 
     @typechecked
     def visit_Name(self, node: Name, env):
-        return self.env[node.name]
+        return env[node.name]
 
     @typechecked
     def visit_UnaryOp(self, node: UnaryOp, env):
-        right_val = self.visit(node.right)
+        right_val = self.visit(node.right, env)
         return UNARY_OPERATORS[node.op](right_val)
 
     @typechecked
     def visit_BinOp(self, node: BinOp, env):
-        left_val = self.visit(node.left)
-        right_val = self.visit(node.right)
+        left_val = self.visit(node.left, env)
+        right_val = self.visit(node.right, env)
         return OPERATORS[node.op](left_val, right_val)
 
     @typechecked
     def visit_ConstDef(self, node: ConstDef, env):
-        self.env[node.name] = node.value
+        env[node.name] = node.value
 
     @typechecked
     def visit_VarDef(self, node: VarDef, env):
         if node.value is not None:
-            self.env[node.name] = self.visit(node.value)
+            env[node.name] = self.visit(node.value, env)
 
     @typechecked
     def visit_Assign(self, node: Assign, env):
         # TODO
-        self.env[node.location] = self.visit(node.value)
+        env[node.location] = self.visit(node.value, env)
 
     @typechecked
     def visit_Print(self, node: Print, env):
-        sys.stdout.write(str(self.visit(node.expression)) + "\n")
+        sys.stdout.write(str(self.visit(node.expression, env)) + "\n")
 
     @typechecked
     def visit_If(self, node: If, env):
-        if self.visit(node.test):
-            return self.visit(node.then)
+        if self.visit(node.test, env):
+            return self.visit(node.then, env)
         else:
-            return self.visit(node.else_)
+            return self.visit(node.else_, env)
 
     @typechecked
     def visit_While(self, node: While, env):
-        while self.visit(node.test):
-            self.visit(node.then)
+        while self.visit(node.test, env):
+            self.visit(node.then, env)
 
     @typechecked
     def visit_Statements(self, node: Statements, env):
         for statement in node.statements:
-            self.visit(statement)
+            self.visit(statement, env)
 
     @typechecked
     def visit_Block(self, node: Block, env):
+        env = env.new_child()
         for statement in node.statements.statements:
-            val = self.visit(statement)
+            val = self.visit(statement, env)
         return val
 
 
