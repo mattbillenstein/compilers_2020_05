08aff67b5bd1bd4a81c7faeb61cde2e3a1cb4d28
Dan Davison <dandavison7@gmail.com>
2020-05-07 15:55:06 -0400

    Start typechecker

--- wabbit/typecheck.py
+++ wabbit/typecheck.py
@@ -1,13 +1,38 @@
-from .model import *
+from collections import ChainMap
+from dataclasses import dataclass
+from .model import Node
+
+from typeguard import typechecked
+
+
+@dataclass
+class TypeChecker:
+    @typechecked
+    def check(self, node, env: ChainMap):
+        method_name = "check_" + node.__class__.__name__
+        if method := getattr(self, method_name, None):
+            method(node, env)
+        else:
+            for attr in [
+                "left",
+                "right",
+                "test",
+                "then",
+                "else_",
+                "value",
+                "location",
+                "expression",
+            ]:
+                if child := getattr(node, attr, None):
+                    self.check(child, env)
+            if child := getattr(node, "statements", None):
+                for statement in node.statements:  # type: ignore
+                    self.check(statement, env)
 
 
 def check_program(model):
-    env = {}
-    check(model, env)
-
-
-def check(node, env):
-    pass
+    env: ChainMap = ChainMap()
+    return TypeChecker().check(model, env)
 
 
 def main(filename):
